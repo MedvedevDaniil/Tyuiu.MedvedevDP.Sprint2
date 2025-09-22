75f418b168a47bac5f78a5fcd8ed3082e303b290
danii <danii@DANIIL>
2025-09-22 17:01:24 +0500

    Add project files.

---
+++ Tyuiu.MedvedevDP.Sprint2.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.MedvedevDP.Sprint2.Task0.V7.Lib;
+
+namespace Tyuiu.MedvedevDP.Sprint2.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService service = new DataService();
+            int x = 103;
+            int y = 475;
+
+            bool[] res = new bool[6];
+
+            res = service.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { true, false, true, true, true, false};
+
+            CollectionAssert.AreEqual(wait,res);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint2.Task0.V7/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MedvedevDP.Sprint2.Task0.V7.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService dataService = new DataService();
+
+        int x = 103;
+        int y = 475;
+
+        bool[] res = new bool[6];
+        res = dataService.GetCompareOperations(x, y);
+
+        for (int i=0; i<6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+    }
+}
