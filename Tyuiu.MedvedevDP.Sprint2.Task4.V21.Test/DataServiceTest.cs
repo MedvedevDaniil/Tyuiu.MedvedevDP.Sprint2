fb4226c6294b289e280188aa6ecc9b7ae16eaa7f
danii <danii@DANIIL>
2025-10-06 17:52:26 +0500

    task4

---
+++ Tyuiu.MedvedevDP.Sprint2.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.MedvedevDP.Sprint2.Task4.V21.Lib;
+namespace Tyuiu.MedvedevDP.Sprint2.Task4.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService dataService = new DataService();
+
+            double x = 1;
+            double y = 2;
+
+            double res = dataService.Calculate(x, y);
+            double wait = 0.719;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint2.Task4.V21/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MedvedevDP.Sprint2.Task4.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService dataService = new DataService();
+
+        Console.WriteLine("Vvedi x:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Vvedi y:");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        double res = dataService.Calculate(x, y);
+
+        Console.WriteLine(res); 
+    }
+}
