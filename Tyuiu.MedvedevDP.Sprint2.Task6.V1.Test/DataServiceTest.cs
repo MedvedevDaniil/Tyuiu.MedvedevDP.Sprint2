2be707452024aed540937e2b8a4edde6f0071045
danii <danii@DANIIL>
2025-10-06 22:38:57 +0500

    task6

---
+++ Tyuiu.MedvedevDP.Sprint2.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.MedvedevDP.Sprint2.Task6.V1.Lib;
+namespace Tyuiu.MedvedevDP.Sprint2.Task6.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindMonthDaysCount_January()
+        {
+            DataService dataService = new DataService();
+            int value = 1;
+            int res = dataService.FindMonthDaysCount(value);
+            int wait = 31;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindMonthDaysCount_February()
+        {
+            DataService dataService = new DataService();
+            int value = 2;
+            int res = dataService.FindMonthDaysCount(value);
+            int wait = 28;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindMonthDaysCount_April()
+        {
+            DataService dataService = new DataService();
+            int value = 4;
+            int res = dataService.FindMonthDaysCount(value);
+            int wait = 30;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindMonthDaysCount_December()
+        {
+            DataService dataService = new DataService();
+            int value = 12;
+            int res = dataService.FindMonthDaysCount(value);
+            int wait = 31;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MedvedevDP.Sprint2.Task6.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService dataService = new DataService();
+
+        Console.WriteLine("Vvedi nomer mesyaca:");
+        int value = Convert.ToInt32(Console.ReadLine());
+
+        int res = dataService.FindMonthDaysCount(value);
+
+        Console.WriteLine(res);
+    }
+}
